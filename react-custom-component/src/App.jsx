14d281a543a033843b4eda3d0651cf3df57c961b
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-10-25 09:21:36 +0530

    first commit

---
+++ react-custom-component/src/App.jsx
@@ -0,0 +1,25 @@
+
+const reactDescriptions = ['Fundamentals', 'Crucial','Core'];
+function genRandomInt(max){
+  return Math.floor(Math.random()*(max+1));
+}
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;

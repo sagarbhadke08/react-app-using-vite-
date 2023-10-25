450654c57a6ff804244c91452512e8ac72ad3d2f
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-10-25 09:26:01 +0530

    improved the code readiability

--- react-custom-component/src/App.jsx
+++ react-custom-component/src/App.jsx
@@ -5,13 +5,15 @@ function genRandomInt(max){
 }
 
 function App() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>

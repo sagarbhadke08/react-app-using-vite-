dcb09f2958e62b45a7048a0cca0f3b80a8c55d1c
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-10-29 19:56:47 +0530

    removed the 'console.log' from code

--- react-custom-component/src/App.jsx
+++ react-custom-component/src/App.jsx
@@ -3,19 +3,18 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/Coreconcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
-
+import { EXAMPLES } from "./data.js";
 function App() {
   // const stateArray = useState('Please click the button');
   // let tabContent = "Please Click the button";
 
-  const [selectedTopic, setSelectedTopic] = useState('Please click the Button');
-
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     // console.log(selectedButton);
     setSelectedTopic(selectedButton);
     // tabContent = selectedButton;
-    console.log(selectedTopic);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -59,11 +58,18 @@ function App() {
             <TabButton onSelect={() => handleSelect("components")}>
               Component
             </TabButton>
-            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {/* {selectedTopic} */}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>

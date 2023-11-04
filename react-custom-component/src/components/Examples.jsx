48d7311e7c686821a741435d7ec39adaa9331082
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-11-04 23:41:01 +0530

    improvised version

--- react-custom-component/src/components/Examples.jsx
+++ react-custom-component/src/components/Examples.jsx
@@ -30,36 +30,37 @@ export default function Examples() {
     );
   }
 
-  <section id="examples">
-    <h2>Examples</h2>
-    <menu>
-      <TabButton
-        isSelected={selectedTopic === "components"}
-        onSelect={() => handleSelect("components")}
-      >
-        Component
-      </TabButton>
-      <TabButton
-        isSelected={selectedTopic === "jsx"}
-        onSelect={() => handleSelect("jsx")}
-      >
-        JSX
-      </TabButton>
-      <TabButton
-        isSelected={selectedTopic === "props"}
-        onSelect={() => handleSelect("props")}
-      >
-        Props
-      </TabButton>
-      <TabButton
-        isSelected={selectedTopic === "state"}
-        onSelect={() => handleSelect("state")}
-      >
-        State
-      </TabButton>
-    </menu>
-    {/* {selectedTopic} */}
-    {/* {!selectedTopic ? <p>Please Select Topic</p> : null}
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Component
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {/* {selectedTopic} */}
+      {/* {!selectedTopic ? <p>Please Select Topic</p> : null}
 
           {selectedTopic ? (
             <div id="tab-content">
@@ -71,13 +72,14 @@ export default function Examples() {
             </div>
           ) : null} */}
 
-    {/* {!selectedTopic ?<p>Please Select Topic</p>: (<div id="tab-content">
+      {/* {!selectedTopic ?<p>Please Select Topic</p>: (<div id="tab-content">
               <h3>{EXAMPLES[selectedTopic].title}</h3>
               <p>{EXAMPLES[selectedTopic].description}</p>
               <pre>
                 <code>{EXAMPLES[selectedTopic].code}</code>
               </pre>
             </div>)} */}
-    {tabContent}
-  </section>;
+      {tabContent}
+    </section>
+  );
 }
